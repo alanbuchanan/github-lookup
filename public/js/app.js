19188546ac75cfb5c4953a4b003934a0a1d88f4e
Rory <rory88@gmail.com>
2015-12-15 19:08:50 +0000

    scaffolded simple view + test functionality for form submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,29 @@
+var Form = React.createClass({
+
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var inputContent = ReactDOM.findDOMNode(this.refs.login);
+        alert(inputContent.value);
+    },
+
+    render: function () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="github username..." ref="login" />
+                <button>Add</button>
+            </form>
+        );
+    }
+});
+
 var Card = React.createClass({
     render: function () {
         return (
-            <div>works</div>
+            <div>
+                <h3>Me</h3>
+                <img src="https://avatars.githubusercontent.com/u/10364894?v=3" width="80"/>
+                <hr/>
+            </div>
         );
     }
 });
@@ -9,7 +31,10 @@ var Card = React.createClass({
 var Main = React.createClass({
     render: function () {
         return (
-            <Card />
+            <div>
+                <Form />
+                <Card />
+            </div>
         );
     }
 });
